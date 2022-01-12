764017ab3b87c1fb25f6d6c743597b1aa3fa329e
Idoia Rojo Lazaro <idoia@ifeelonline.com>
2022-01-12 12:14:50 +0100

    iframe axa

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  const url_staging = process.env.REACT_APP_URL;
+
+  return (
+    <div className="App">
+      <iframe
+        title="iframe-axa"
+        allow="camera *;microphone *"
+        width="100%"
+        height="700"
+        src={url_staging}
+        id="co-iframe"
+        name="co-iframe"
+        sandbox="allow-same-origin allow-scripts"
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
